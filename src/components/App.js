4849ee8e7f593788ec8c9151c369cc7058691996
Nikhil77sutara <nikhilchoudhary@sutara.org>
2023-11-11 21:20:03 +0530

    first

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,16 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [tab,setTab] = useState("Tab 1");
   return (
     <div>
         {/* Do not remove the main div */}
+        <li onClick={()=>{setTab("Tab 1")}}>Tab 1</li>
+        <li onClick={()=>{setTab("Tab 2")}}>Tab 2</li>
+        <li onClick={()=>{setTab("Tab 3")}}>Tab 3</li>
+        <p>This is the content for {tab}</p>
     </div>
   )
 }
